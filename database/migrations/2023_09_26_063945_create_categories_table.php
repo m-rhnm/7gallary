c39e2c40a151d1cbbc17ed1a9478005b72c7a3e4
m-rhnm <mohamadrahnama1516@gmail.com>
2023-09-26 11:25:43 +0330

    all tables created

--- database/migrations/2023_09_26_063945_create_categories_table.php
+++ database/migrations/2023_09_26_063945_create_categories_table.php
@@ -11,7 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->char('slug',255);
+            $table->char('title',255);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('categories');
     }
 };
---
+++ database/migrations/2023_09_26_074648_creat_payments__table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('ref_id');
+            $table->unsignedInteger('res_id');
+            $table->enum('getaway',['zarinpal','idpay']);
+            $table->enum('status',['paied','unpaid']);
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
