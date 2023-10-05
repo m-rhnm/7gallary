4da2e339b7769d156c6a01942ae410868fd0335d
m-rhnm <mohamadrahnama1516@gmail.com>
2023-10-05 14:03:19 +0330

    users section compaltly implemented

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Users\StoreRequest;
+use App\Http\Requests\Admin\Users\UpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,18 +15,48 @@ class UsersController extends Controller
     }
     public function store(StoreRequest $request){
         $validation_data=$request->validated();
-        $createdCategory = User::create([
+        $createdUser = User::create([
+            'name' =>  $validation_data['name'],
+            'email' =>  $validation_data['email'],
+            'mobile' =>  $validation_data['mobile'],
+            'role' =>  $validation_data['role'],
         
         ]);
-        if(!$createdCategory){
-         return back()->with('failed','failed creating category');
+        if(!$createdUser){
+         return back()->with('failed','failed  to creating new user');
         }
-        return back()->with('success',"Successfully created category");
+        return back()->with('success',"Successfully created new user");
      }
     
     public function all(){
        $users = User::paginate(2);
          return view('admin.users.all',compact('users'));
+    }
+    public function delete($user_id){
+        $deleteUser = User::findOrFail($user_id);
+        $deleteUser->delete();
+        return back()->with('success',"Successfully deleted user");
+    }
+    public function edit($user_id){
+        $users = User::findOrFail($user_id);
+        return view('admin.users.edit',compact('users'));
+
+    }
+
+    public function update(UpdateRequest $request,$user_id){
+        $validation_data=$request->validated();
+         $userUpdated=$user = User::find($user_id);
+        $user->update([
+            'name' =>  $validation_data['name'],
+            'email' =>  $validation_data['email'],
+            'mobile' =>  $validation_data['mobile'],
+            'role' =>  $validation_data['role'],
+        ]);
+        if(!$userUpdated){
+            return back()->with('failed','failed to update user');
+           }
+           return back()->with('success',"Successfully updated user");
+        
     }
 
 
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.admin.master')
 
-
 @section('content')
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -12,8 +10,8 @@
           <div class="col-12">
             <h1 class="m-0 text-dark">
                 <a class="nav-link drawer" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-                کاربران / افزودن
-                <a class="btn btn-primary float-left text-white py-2 px-4" href="{{ route('admin.users.all') }}">بازگشت به صفحه کاربران</a>
+                USERS/EDIT
+                <a class="btn btn-primary float-left text-white py-2 px-4" href="{{ route('admin.users.all') }}">Back to users page</a>
             </h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -29,34 +27,36 @@
               <div class="col-md-12">
                   <div class="card card-defualt">
                       <!-- form start -->
-                      <form action="{{ route('admin.users.store') }}" method="post">
+                      <form action="{{ route('admin.users.update',$users->id) }}" method="post">
                         @csrf
-                          <div class="card-body">
+                        @method('PUT')
+                      <div class="card-body">
                               <div class="row">
                                   <div class="col-md-6">
                                       <div class="form-group">
                                           <label>نام و نام خانوادگی</label>
-                                          <input type="text" class="form-control" name="name" placeholder="نام و نام خانوادگی را وارد کنید">
+                                          <input type="text" class="form-control" name="name" placeholder="نام و نام خانوادگی را وارد کنید" value="{{ $users->name }}">
                                       </div>
                                   </div>
                                   <div class="col-md-6">
                                       <div class="form-group">
                                           <label>ایمیل</label>
-                                          <input type="email" class="form-control" name="email" placeholder="ایمیل را وارد کنید">
+                                          <input type="email" class="form-control" name="email" placeholder="ایمیل را وارد کنید" value="{{ $users->email }}">
                                       </div>
                                   </div>
                                   <div class="col-md-6">
                                       <div class="form-group">
                                           <label>موبایل</label>
-                                          <input type="text" class="form-control" name="mobile" placeholder="موبایل را وارد کنید">
+                                          <input type="text" class="form-control" name="mobile" placeholder="موبایل را وارد کنید" value="{{ $users->mobile }}">
                                       </div>
                                   </div>
                                   <div class="col-md-6">
                                       <div class="form-group">
                                           <label>نقش کاربری</label>
                                           <select class="form-control" name="role">
-                                              <option value="user"  >کاربر عادی</option>
-                                              <option value="admin" >مدیر</option>
+                                              <option value="user"> user</option>
+                                              <option value="admin"> admin </option>
+
                                           </select>
                                       </div>
                                   </div>
